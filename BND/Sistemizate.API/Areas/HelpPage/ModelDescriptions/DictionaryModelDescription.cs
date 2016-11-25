eca4343182e30af2f73d63e0ba7ddc6a05ecd6c2
Oss Silva <osilvardz@outlook.com>
2016-11-25 01:51:45 -0600

    First implementation of Entity Framework and Web API projects.

---
+++ BND/Sistemizate.API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Sistemizate.API.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ BND/Sistemizate.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sistemizate.API.WebApiApplication" Language="C#" %>
